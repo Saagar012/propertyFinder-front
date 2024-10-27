7888113d3461b8d4a48e41449a93e52587554281
Sagar KC <sagar.kc.a18@gmail.com>
2024-10-26 21:56:45 -0400

    login and signup is completed in frontend

---
+++ src/app/core/services/static-data.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class StaticDataService {
+  public readonly USER_TYPE = {
+    ADMIN: '0',
+    USER: '1',
+  };
+}
+
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false
 };
-export const baseUrl = `http://localhost:3000/`;
+export const baseUrl = `http://localhost:3000/api/v1/auth/`;
 
 /*
  * For easier debugging in development mode, you can import the following file
